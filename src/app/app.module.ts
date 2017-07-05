4c067241256f68228a42321755016d2a00479118
hinesboy <hines.zhu@gmail.com>
2017-07-05 14:04:09 +0800

    add text result dir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import {TextDemoComponent} from "./text_demo/text_demo.component";
+import  { TextResultComponent } from './text_result/text.result.component'
 import  { AppRoutingModule } from './app-routing.module'
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TextDemoComponent
+    TextDemoComponent,
+    TextResultComponent
   ],
   imports: [
     AppRoutingModule,
---
+++ src/app/text_result/text.result.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/7/5 0005.
+ */
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'text-result',
+  styleUrls: ['./text.result.component.css'],
+  templateUrl: './text.result.component.html'
+})
+export class TextResultComponent {
+}
