ffe31ded85cd5d68de4355c048533f774051ad8a
kuangYIN1217 <519997474@qq.com>
2017-07-05 13:48:05 +0800

    text demo

--- src/app/text_demo/text_demo.component.ts
+++ src/app/text_demo/text_demo.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core'
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'text_demo',
   styleUrls: ['./css/text_demo.component.css'],
   templateUrl: './templates/text_demo.html'
 })
 export class TextDemoComponent {
+  textShow:number=1;
   constructor() {
-    console.log(1);
+  }
+  textDemo(){
+    this.textShow=1;
+  }
+  uploadText(){
+    this.textShow=2;
+  }
+  bigText(){
+    this.textShow=3;
   }
 }
 
