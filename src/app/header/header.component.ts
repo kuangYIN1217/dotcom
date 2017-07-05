d5828bd93a8fd5b549ef9ba5c732d510108d6ebd
hinesboy <hines.zhu@gmail.com>
2017-07-05 09:06:08 +0800

    init

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2017/7/4 0004.
+ */
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-header',
+  styleUrls: ['./header.component.css'],
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+}
+
