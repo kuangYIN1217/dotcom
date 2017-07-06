ec6fec291aa7336191e39164931775f5d60a1f8d
hinesboy <hines.zhu@gmail.com>
2017-07-06 09:26:05 +0800

    merge

---
+++ src/app/text_result/word-analysis/word.analysis.component.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2017/7/6 0006.
+ */
+/**
+ * Created by Administrator on 2017/7/5 0005.
+ */
+import {Component} from '@angular/core'
+@Component({
+  selector: 'ele-word-analysis',
+  styleUrls: ['./word.analysis.component.css'],
+  templateUrl: './word.analysis.component.html'
+})
+export class WordAnalysisComponent {
+
+}
